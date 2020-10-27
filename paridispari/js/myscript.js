7d579f470814a8f607797e111e50ba1dbaf297f7
Anna <rosalenanna@gmail.com>
2020-10-27 17:39:20 +0100

    ordine commenti

--- paridispari/js/myscript.js
+++ paridispari/js/myscript.js
@@ -15,16 +15,19 @@ var numeroPc = Math.floor(Math.random() *5) + 1;
 console.log(numeroPc);
 
 
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) e dichiariamo chi ha vinto
+// creo la funzione
 function funzione(numb1, numb2){
+  // se errore utente primo prompt
   if (pariodispari != "pari" && pariodispari != "dispari") {
     return "ERRORE! : non hai scelto pari o dispari";
   }
 
+  // se errore utente secondo prompt
   if (numeroUte < 1 || numeroUte > 5) {
     return "ERRORE! : non hai inserito un numero da 1 a 5";
   }
 
+  // Stabiliamo se la somma dei due numeri è pari o dispari e dichiariamo chi ha vinto
   var risultato = numb1 + numb2;
   if (risultato % 2 === 0 && pariodispari === "pari") {
     return "hai vinto";

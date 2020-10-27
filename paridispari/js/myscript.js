8f0ed62de413ea57a8373e2f01237874376b0ca6
Anna <rosalenanna@gmail.com>
2020-10-27 16:40:45 +0100

    script pari dispari

---
+++ paridispari/js/myscript.js
@@ -0,0 +1,27 @@
+// ESERCIZIO Pari e Dispari
+
+// L’utente sceglie pari o dispari
+var pariodispari = prompt("scrivi pari o dispari");
+console.log(pariodispari);
+
+// utente inserisce un numero da 1 a 5.
+var numeroUte = parseInt(prompt("inserisci un numero da 1 a 5"));
+console.log(numeroUte);
+
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+var numeroPc = Math.floor(Math.random() *5) + 1;
+console.log(numeroPc);
+
+
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) e dichiariamo chi ha vinto
+function funzione(numb1, numb2){
+  var risultato = numb1 + numb2;
+  if (risultato % 2 === 0 && pariodispari === "pari") {
+    return "hai vinto";
+  }else{
+    return "ha vinto il computer";
+  }
+}
+
+var somma = funzione(numeroUte, numeroPc);
+console.log(somma);

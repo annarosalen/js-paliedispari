2a64267fc99e51c67d0febe1ab06a0a0022c8f1a
Anna <rosalenanna@gmail.com>
2020-10-27 17:36:01 +0100

    aggiunta errore  utente

--- paridispari/js/myscript.js
+++ paridispari/js/myscript.js
@@ -8,6 +8,8 @@ console.log(pariodispari);
 var numeroUte = parseInt(prompt("inserisci un numero da 1 a 5"));
 console.log(numeroUte);
 
+
+
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 var numeroPc = Math.floor(Math.random() *5) + 1;
 console.log(numeroPc);
@@ -15,6 +17,14 @@ console.log(numeroPc);
 
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) e dichiariamo chi ha vinto
 function funzione(numb1, numb2){
+  if (pariodispari != "pari" && pariodispari != "dispari") {
+    return "ERRORE! : non hai scelto pari o dispari";
+  }
+
+  if (numeroUte < 1 || numeroUte > 5) {
+    return "ERRORE! : non hai inserito un numero da 1 a 5";
+  }
+
   var risultato = numb1 + numb2;
   if (risultato % 2 === 0 && pariodispari === "pari") {
     return "hai vinto";
